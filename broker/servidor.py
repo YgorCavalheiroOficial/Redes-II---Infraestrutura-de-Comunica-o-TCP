b1374ae94c344697728144d12574a128fad7cdbd
ycavalheiro <ygorcavalheiro93@gmail.com>
2026-05-02 12:36:18 -0300

    Atualização do Publish Subscriber no Broker

--- broker/servidor.py
+++ broker/servidor.py
@@ -7,10 +7,14 @@ class Broker:
     def __init__(self, host='127.0.0.1', port=5000):
         self.host = host
         self.port = port
-        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.topicos = {}  # Passo 4: Dicionário para gerenciar tópicos e inscritos { 'Topic A': [conn1, conn2] }
         
-        # Chaves do Broker
+        # Configuração do Socket TCP (IPv4, Stream/TCP)
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        
+        # Dicionário para gerenciar tópicos e inscritos { 'Topic A': [conn1, conn2] }
+        self.topicos = {}  
+       
+        # Chaves do Broker para implementação da criptografia nas próximas entregas
         self.chave_privada, self.chave_publica = CryptoUtils.gerar_par_chaves()
         
     def start(self):
@@ -20,59 +24,73 @@ class Broker:
         
         while True:
             conn, addr = self.server_socket.accept()
+            # Inicia uma thread para cada cliente conectado
             thread = threading.Thread(target=self.handle_client, args=(conn, addr))
             thread.start()
 
     def handle_client(self, conn, addr):
-        print(f"Nova conexão TCP (Passo 1): {addr}")
+        print(f"Novo cliente conectado: {addr}")
+        buffer = ""
         
         try:
-            # Passo 1: Recebimento do "Olá" inicial
+            # Passo 1: Apresentação do cliente
             msg_inicial = conn.recv(1024).decode()
             if msg_inicial == "HELLO_BROKER":
                 conn.send("HELLO_CLIENT".encode())
 
-            # Passo 2 & 3: Handshake, Envelopamento e Autenticação (Simplificado no modelo)
-            # Na prática real, você trocaria chaves públicas, validaria o certificado do cliente e receberia a chave simétrica cifrada.
-            print(f"Realizando Handshake e validando assinaturas com {addr}...")
+            # Passo 2 & 3: Handshake, Envelopamento e Autenticação para a próxima entrega
+            # print(f"Realizando Handshake e validando assinaturas com {addr}...")
             
-            # Loop principal de recebimento de comandos do cliente
+            # Loop principal para ler as mensagens
             while True:
                 data = conn.recv(2048)
                 if not data:
-                    break
+                    break # Ocorre quando o cliente desconecta.
                 
-                # Assume-se que a mensagem aqui já foi decifrada com a chave simétrica do envelopamento
-                comando = json.loads(data.decode())
+                # TCP Stream: junta os pedaços e separa por quebra de linha
+                buffer += data.decode()
+                mensagens = buffer.splint('\n')
+
+                # O último elemento é sempre vazio ou incompleto, volta pro buffer
+                buffer = mensagens.pop()
                 
-                # Passo 4: Lógica de Tópicos
-                if comando['acao'] == 'SUBSCRIBE':
-                    topico = comando['topico']
-                    if topico not in self.topicos:
-                        self.topicos[topico] = []
-                    self.topicos[topico].append(conn)
-                    print(f"Cliente {addr} inscrito no {topico}")
+                for msg_str in mensagens:
+                    if not msg_str.strip(): continue
+
+                    comando = json.loads(msg_str)
                     
-                # Passo 5: Confidencialidade Ponta a Ponta
-                elif comando['acao'] == 'PUBLISH':
-                    topico = comando['topico']
-                    payload_cifrado = comando['payload_cifrado']
-                    # O Broker apenas repassa o payload_cifrado, não consegue ler o conteúdo
-                    print(f"Repassando pacote cifrado ponta a ponta no {topico}...")
-                    self.notificar_inscritos(topico, payload_cifrado, remetente=conn)
+                    # Passo 4: Lógica de Tópicos
+                    if comando['acao'] == 'SUBSCRIBE':
+                        topico = comando['topico']
+                        if topico not in self.topicos:
+                            self.topicos[topico] = []
+                        self.topicos[topico].append(conn)
+                        print(f"Cliente {addr} inscrito no {topico}")
+                        
+                    # Passo 5: Confidencialidade Ponta a Ponta
+                    elif comando['acao'] == 'PUBLISH':
+                        topico = comando['topico']
+                        payload = comando['payload']
+                        # O Broker apenas repassa o payload, não consegue ler o conteúdo
+                        print(f"Repassando pacote cifrado ponta a ponta no '{topico}': {payload}...")
+                        self.notificar_inscritos(topico, payload, remetente=conn)
 
         except Exception as e:
             print(f"Erro na conexão com {addr}: {e}")
         finally:
+            # Limpeza caso o cliente desconecte
+            for topico, clientes in self.topicos.items():
+                if conn in clientes:
+                    clientes.remove(conn)
             conn.close()
 
-    def notificar_inscritos(self, topico, payload_cifrado, remetente):
+    def notificar_inscritos(self, topico, payload, remetente):
         if topico in self.topicos:
             mensagem = json.dumps({
                 'acao': 'RECEIVE',
                 'topico': topico,
-                'payload_cifrado': payload_cifrado
-            }).encode()
+                'payload': payload
+            }) +"\n".encode()
             
             for cliente in self.topicos[topico]:
                 if cliente != remetente: # Não envia de volta para quem publicou
