f61d6e1bcd281aefdc1a64e6e3dbed7fb5c32590
ycavalheiro <ygorcavalheiro93@gmail.com>
2026-05-03 02:02:44 -0300

    Adaptação do arquivo aplicacao_cliente.py para funcionamento do modelo Pub/Sub sem criptografia (para a primeira entrega)

--- cliente/aplicacao_cliente.py
+++ cliente/aplicacao_cliente.py
@@ -2,6 +2,7 @@ import json
 import socket
 import threading
 from utils.crypto_utils import CryptoUtils
+from cryptography.fernet import Fernet
 
 class Cliente:
     def __init__(self, broker_host='127.0.0.1', broker_port=5000):
@@ -9,62 +10,70 @@ class Cliente:
         self.broker_port = broker_port
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         
-        # Chaves do Cliente
+
+        # Geração das chaves do cliente: pré estruturado para a implementação da criptografia para as próximas entregas
         self.chave_privada, self.chave_publica = CryptoUtils.gerar_par_chaves()
         self.chave_simetrica_envelopamento = CryptoUtils.gerar_chave_simetrica()
-        
-        # Chave para Criptografia Ponta a Ponta (Compartilhada apenas entre clientes previamente)
-        # Em um cenário real, você faria a troca de chaves E2E usando Diffie-Hellman ou similar.
+        # Chave para Criptografia Ponta a Ponta Compartilhada apenas entre clientes previamente
         self.chave_e2e = Fernet(Fernet.generate_key()) 
 
     def conectar(self):
-        # Passo 1: Conexão TCP Básica
+        #Conexão TCP Básica
         self.socket.connect((self.broker_host, self.broker_port))
-        self.socket.send("HELLO_BROKER".encode())
+        #Envio da saudação com o limitador
+        self.socket.send("HELLO_BROKER\n".encode())
         resposta = self.socket.recv(1024).decode()
-        print(f"Resposta do servidor: {resposta}")
+        print(f"Conectado! Resposta do Servidor: {resposta.strip()}")
         
-        # Passo 2 & 3: Aqui ocorreria a troca da chave_simetrica_envelopamento 
-        # cifrada com a chave pública do Broker (Envelopamento Digital) e o envio
-        # do certificado assinado pelo Broker para autenticação.
+        # Passo 2 & 3: Local destinado para realizar a troca da chave_simetrica_envelopamento para as próximas entregas
+        # cifrada com a chave pública do Broker (Envelopamento Digital) e o envio do certificado assinado pelo Broker para autenticação.
         
         # Inicia thread para escutar mensagens do Broker
         threading.Thread(target=self.escutar, daemon=True).start()
 
     def inscrever(self, topico):
-        # Passo 4: Solicitação de entrada em um tópico
-        msg = json.dumps({'acao': 'SUBSCRIBE', 'topico': topico})
-        # Esta mensagem deveria ser cifrada com a chave simétrica do envelopamento antes do envio
-        self.socket.send(msg.encode())
+        #Solicitação de entrada em um tópico
+        pacote = json.dumps({'acao': 'SUBSCRIBE', 'topico': topico}) + "\n"
+        # Futuramente esta mensagem será cifrada com a chave simétrica do envelopamento antes do envio
+        self.socket.send(pacote.encode())
 
     def publicar(self, topico, mensagem_clara):
-        # Passo 5: Confidencialidade Ponta a Ponta
+        # Passo 5: Futuramente será implementada a Confidencialidade Ponta a Ponta
         # Cifra o payload antes de colocar no pacote do Broker
-        payload_cifrado = self.chave_e2e.encrypt(mensagem_clara.encode()).decode()
+        #payload_cifrado = self.chave_e2e.encrypt(mensagem_clara.encode()).decode()
         
-        msg = json.dumps({
+        pacote = json.dumps({
             'acao': 'PUBLISH', 
             'topico': topico, 
-            'payload_cifrado': payload_cifrado
-        })
-        # O cabeçalho ('acao', 'topico') fica visível para o Broker fazer o roteamento, 
-        # mas o 'payload_cifrado' é ilegível para ele.
-        self.socket.send(msg.encode())
+            'payload': mensagem_clara
+        }) + "\n"
+        self.socket.send(pacote.encode())
 
     def escutar(self):
+        buffer = "" #Pesquisar o motivo lógico pelo qual o buffer deve ser inicializado vazio
         while True:
             try:
                 data = self.socket.recv(2048)
-                if data:
-                    pacote = json.loads(data.decode())
+                if not data:
+                    print("Conexão com o Broker foi encerrada.")
+                    break
+                
+                # Lida com o TCP Stream da mesma forma que o servidor
+                buffer += data.decode()
+                mensagens = buffer.split('\n')
+                buffer = mensagens.pop()
+                
+                for msg_str in mensagens:
+                    if not msg_str.strip(): continue
+                    
+                    pacote = json.loads(msg_str)
+                    
                     if pacote['acao'] == 'RECEIVE':
-                        # Decifra o pacote ponta a ponta
-                        payload_cifrado = pacote['payload_cifrado'].encode()
-                        try:
-                            msg_decifrada = self.chave_e2e.decrypt(payload_cifrado).decode()
-                            print(f"\n[Mensagem Recebida no {pacote['topico']}] (Descriptografada): {msg_decifrada}")
-                        except Exception:
-                            print("\n[Erro] Falha ao decifrar mensagem E2E. Chaves não correspondem.")
+                        # ADAPTAÇÃO FUTURA: em breve será usado o self.chave_e2e.decrypt() para decodificar a mensagem previamente criptografada.
+                        topico = pacote['topico']
+                        mensagem = pacote['payload']
+                        print(f"\n[Nova Mensagem no tópico '{topico}']: {mensagem}")
+                        
             except Exception as e:
-                print("Conexão encerrada.")
+                print(f"Erro na escuta: {e}")
                 break
